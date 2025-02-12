d9c86a330fe4e5b1a1333fad6c827b9c19a31e32
Hugo bessa <hugo@vinta.com.br>
2025-02-12 02:49:57 -0300

    Initial commit

---
+++ winston-logger.ts
@@ -0,0 +1,29 @@
+import type { BaseLogger } from 'vintasend/src/services/loggers/base-logger';
+
+import * as winston from 'winston';
+
+export class WinstonLogger implements BaseLogger {
+  private logger: winston.Logger;
+
+  constructor(winstonOptions?: Parameters<typeof winston.createLogger>[0]) {
+    this.logger = winston.createLogger(winstonOptions || {});
+
+    if (process.env.NODE_ENV === 'development') {
+      this.logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+      }));
+    }
+  }
+
+  info(message: string): void {
+    this.logger.info(message);
+  }
+
+  error(message: string): void {
+    this.logger.error(message);
+  }
+
+  warn(message: string): void {
+    this.logger.warn(message);
+  }
+}
