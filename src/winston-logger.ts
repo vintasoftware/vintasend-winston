a1ae94ff8bd4e39d308028c5eee1baf16a39b780
Hugo bessa <hugo@vinta.com.br>
2025-03-05 02:12:03 -0300

    Version bump

--- src/winston-logger.ts
+++ src/winston-logger.ts
@@ -1,4 +1,4 @@
-import type { BaseLogger } from 'vintasend/src/services/loggers/base-logger';
+import type { BaseLogger } from 'vintasend/dist/services/loggers/base-logger';
 
 import * as winston from 'winston';
 
